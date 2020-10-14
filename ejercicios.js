d1438e3388fb0bb86321998e91f4e9d44af37411
lujancuellar <67484949+lujancuellar@users.noreply.github.com>
2020-10-14 17:08:08 -0300

    Añado las primeras dos funciones de jquery

---
+++ ejercicios.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    alert ("READY!");
+});
+
+$(document).ready(function() {
+    $("h1").text("Este texto se vera cuando la pagina este completamente cargada");
+});
