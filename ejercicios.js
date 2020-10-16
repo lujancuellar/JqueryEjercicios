c7c20aedeae7e3e315c425f380572f35bcfd6300
lujancuellar <67484949+lujancuellar@users.noreply.github.com>
2020-10-16 11:37:08 -0300

    Resuelto ejercicio de fuentes

--- ejercicios.js
+++ ejercicios.js
@@ -4,10 +4,6 @@ $(document).ready(function() {
     alert ("READY!");
 });
 
-$(document).ready(function() {
-    $("h1").text("Este texto se vera cuando la pagina este completamente cargada");
-});
-
 //Crear un boton "Mostrar contenido oculto", que muestre un texto al hacer click.
 $(document).ready(function(){ 
     $('#mostrar').on('click',function(){
@@ -27,3 +23,64 @@ $(document).ready(function(){
         i = (i + 1) % myColors.length;
     });
 });
+
+//ejercicio de añadir parrafo al final. Los dos hacen 
+
+$(document).ready(function(){
+    $("#boton").click(function(){
+      $("body").append("<p>Esto es un parrafo agregado al final del body</p>");
+    });
+  });
+
+//Ejercicio de Google Fonts.
+
+
+$(document).ready(function() {
+    $("h1").on({
+      click: function() {
+         $(".fuente").css({"font-family": "Roboto"});
+      }
+    });    
+ });
+
+ $(document).ready(function() {
+    $("h2").on({
+      click: function() {
+         $(".fuente").css({"font-family": "Sansita Swashed"});
+      }
+    });    
+ });
+
+ 
+ $(document).ready(function() {
+    $("h3").on({
+      click: function() {
+         $(".fuente").css({"font-family": "Piedra, cursive"});
+      }
+    });    
+ });
+
+ $(document).ready(function() {
+    $("h4").on({
+      click: function() {
+         $(".fuente").css({"font-family": "Koulen"});
+      }
+    });    
+ });
+
+ $(document).ready(function() {
+    $("h5").on({
+      click: function() {
+         $(".fuente").css({"font-family": "Chilanka"});
+      }
+    });    
+ });
+
+ $(document).ready(function() {
+    $("h6").on({
+      click: function() {
+         $(".fuente").css({"font-family": "Krona One, Sans Serif"});
+      }
+    });    
+ });
+ 
